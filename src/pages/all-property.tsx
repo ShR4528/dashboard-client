bbac81c9fd867bf3259450391a37e365937883db
Shr4528 <Shr4528@gmail.com>
2023-02-15 15:17:51 +0200

    Form

--- src/pages/all-property.tsx
+++ src/pages/all-property.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Add } from '@mui/icons-material'
+import { useList } from '@pankod/refine-core'
+import { Box, Stack, Typography } from '@pankod/refine-mui'
+import { useNavigate } from '@pankod/refine-react-router-v6'
 
-const allProperty = () => {
-  return <div>all-property</div>
+import { PropertyCard, CustomButton } from 'components'
+
+const AllProperty = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Box>
+      <Stack direction='row' justifyContent='space-between' alignItems='center'>
+        <Typography fontSize={25} fontWeight={700} color='#11142d'>
+          AllProperties
+        </Typography>
+        <CustomButton
+          title='Add Property'
+          handleClick={() => navigate('/properties/create')}
+          backgroundColor='#475be8'
+          color='#fcfcfc'
+          icon={<Add />}
+        />
+      </Stack>
+    </Box>
+  )
 }
 
-export default allProperty
+export default AllProperty
