84cbd377fbee4c4a6c28782c5e08392405d20aa3
Shr4528 <Shr4528@gmail.com>
2023-02-17 18:33:39 +0200

    sorting

--- src/pages/all-property.tsx
+++ src/pages/all-property.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Add } from '@mui/icons-material';
 import { useTable } from '@pankod/refine-core';
 import {
@@ -29,6 +29,24 @@ const AllProperties = () => {
 
   const allProperties = data?.data ?? [];
 
+  const currentPrice = sorter.find((item) => item.field === 'price')?.order;
+
+  const toggleSort = (field: string) => {
+    setSorter([{ field, order: currentPrice === 'asc' ? 'desc' : 'asc' }]);
+  };
+
+  const currentFilterValues = useMemo(() => {
+    const logicalFilters = filters.flatMap((item) =>
+      'field' in item ? item : []
+    );
+    return {
+      title: logicalFilters.find((item) => item.field === 'title')?.value || '',
+      propertyType:
+        logicalFilters.find((item) => item.field === ' propertyType')?.value ||
+        '',
+    };
+  }, [filters]);
+
   if (isLoading) return <Typography>Loading ...</Typography>;
   if (isError) return <Typography>Error ...</Typography>;
 
@@ -53,8 +71,8 @@ const AllProperties = () => {
                 flexWrap='wrap'
                 mb={{ xs: '20px', sm: 0 }}>
                 <CustomButton
-                  title={`Sort price`}
-                  handleClick={() => {}}
+                  title={`Sort price ${currentPrice === 'asc' ? '↑' : '↓'}`}
+                  handleClick={() => toggleSort('price')}
                   backgroundColor='#475be8'
                   color='#fcfcfc'
                 />
@@ -62,8 +80,18 @@ const AllProperties = () => {
                   variant='outlined'
                   color='info'
                   placeholder='Search by title'
-                  value=''
-                  onChange={() => {}}
+                  value={currentFilterValues.title}
+                  onChange={(e) => {
+                    setFilters([
+                      {
+                        field: 'title',
+                        operator: 'contains',
+                        value: e.currentTarget.value
+                          ? e.currentTarget.value
+                          : undefined,
+                      },
+                    ]);
+                  }}
                 />
                 <Select
                   variant='outlined'
@@ -71,9 +99,34 @@ const AllProperties = () => {
                   displayEmpty
                   required
                   inputProps={{ 'aria-label': 'Without label' }}
-                  defaultValue=''
-                  onChange={() => {}}>
-                  <MenuItem>All</MenuItem>
+                  defaultValue={currentFilterValues.propertyType}
+                  onChange={(e) => {
+                    setFilters(
+                      [
+                        {
+                          field: 'propertyType',
+                          operator: 'eq',
+                          value: e.target.value,
+                        },
+                      ],
+                      'replace'
+                    );
+                  }}>
+                  <MenuItem value=''>All</MenuItem>
+                  {[
+                    'Apartment',
+                    'Villa',
+                    'Farmhouse',
+                    'Condos',
+                    'Townhouse',
+                    'Duplex',
+                    'Studio',
+                    'Chalet',
+                  ].map((type) => (
+                    <MenuItem key={type} value={type.toLowerCase()}>
+                      {type}
+                    </MenuItem>
+                  ))}
                 </Select>
               </Box>
             </Box>
@@ -127,6 +180,22 @@ const AllProperties = () => {
             color='#fcfcfc'
             disabled={current === pageCount}
           />
+          <Select
+            variant='outlined'
+            color='info'
+            displayEmpty
+            required
+            inputProps={{ 'aria-label': 'Without label' }}
+            defaultValue={10}
+            onChange={(e) =>
+              setPageSize(e.target.value ? Number(e.target.value) : 10)
+            }>
+            {[10, 20, 30, 40, 50].map((size) => (
+              <MenuItem key={size} value={size}>
+                Show {size}
+              </MenuItem>
+            ))}
+          </Select>
         </Box>
       )}
     </Box>
